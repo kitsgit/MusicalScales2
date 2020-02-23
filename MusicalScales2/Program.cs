ea1075208f496bd0a2aec21253fe51619ec4f5ae
kitsgit <christina.aswez@gmail.com>
2020-02-23 22:25:25 +0530

    roughly completed

--- MusicalScales2/Program.cs
+++ MusicalScales2/Program.cs
@@ -13,51 +13,87 @@ namespace MusicalScales2
     {
         static void Main(string[] args)
         {
-            string scale_name="", scale_formula="";
+            string scale_name="", scale_formula="",root,choice;
+            Console.WriteLine("The available scales are : \n");
             string path = "C:\\Users\\admin.INL082\\source\\repos\\MusicalScales2\\Scales\\vector.txt";
             FileStream fs = new FileStream(path, FileMode.Open);
             BinaryFormatter formatter = new BinaryFormatter();
             Hashtable scalehash = null;
 
             scalehash = (Hashtable)formatter.Deserialize(fs);
-
+            fs.Close();
             foreach (string key in scalehash.Keys)
             {
                 Console.WriteLine(String.Format("{0}: {1}", key, scalehash[key]));
             }
-
+            Console.WriteLine();
             int q = 1;
             while (q ==1)
             {
 
-                Console.WriteLine("Enter the scale : ");
+                Console.WriteLine("Enter the scale/search : ");
                 string input_scale = Console.ReadLine();
                 if (scalehash.ContainsKey(input_scale))
                 {
+                    int q2 = 1;
+                    while (q2 == 1)
+                    {
+                        Console.WriteLine("Enter the root : ");
+                        root = Console.ReadLine();
 
-                    Console.WriteLine(scalehash[input_scale]);
-                    Scale s = new Scale(input_scale, scalehash[input_scale].ToString());
-                    s.ToString();
-                    q = 0;
+                      //  Console.WriteLine(scalehash[input_scale]);
+                        Scale s = new Scale(input_scale, scalehash[input_scale].ToString(),root);
+                        s.Print();
+                        Console.WriteLine("Change scale? y/n");
+                        choice = Console.ReadLine();
+                        if(choice=="y")
+                        {
+                            q2 = 0;
+                        }
+                        else if(choice == "n")
+                        {
+                            q = 1;
+                        }
+                    }
+                  //  q = 0;
                     
                 }
                 else //if (!scalehash.ContainsKey(input_scale))
                 {
                     Console.WriteLine("Key does not exist. Do you want to add? y/n");
-                    string choice = Console.ReadLine();
+                    choice = Console.ReadLine();
                     if (choice == "y")
                     {
-                        Console.WriteLine("Enter the name : ");
-                        scale_name = Console.ReadLine();
-                        Console.WriteLine("Enter the formula : ");
-                        scale_formula = Console.ReadLine();
-                        scalehash.Add(scale_name, scale_formula);
-                        formatter.Serialize(fs, scalehash);
+                        int q3 = 1;
+                        while (q3 == 1)
+                        {
+                            fs = new FileStream(path, FileMode.Open);
+                            Console.WriteLine("Enter the name : ");
+                            scale_name = Console.ReadLine();
+                            Console.WriteLine("Enter the formula : ");
+                            scale_formula = Console.ReadLine();
+                            scalehash.Add(scale_name, scale_formula);
+                            formatter.Serialize(fs, scalehash);
+                            fs.Close();
+                            Console.WriteLine("Continue entering scales? y/n");
                         
-                        q = 0;
+                            choice = Console.ReadLine();
+                            if (choice == "y")
+                            {
+                                //q = 0;
+                                q3 = 1;
+                            }
+                            else if (choice == "n")
+                            {
+                              //  q = 1;
+                                q3 = 0;
+                            }
+                            
+                        }
+                        
                     }
                     
-                    else
+                    else if(choice=="n")
                     {
                         q = 1;
                     }
@@ -72,8 +108,8 @@ namespace MusicalScales2
             //s.name = "major";
             //s.formula = "2212221";
 
-
-          //  fs = new FileStream(path, FileMode.Open);
+            fs.Close();
+            //  fs = new FileStream(path, FileMode.Open);
             System.IO.File.WriteAllText(path, "");
             fs = new FileStream(path, FileMode.Open);
             formatter.Serialize(fs, scalehash);
--- MusicalScales2/Scale.cs
+++ MusicalScales2/Scale.cs
@@ -9,22 +9,33 @@ namespace MusicalScales2
     [Serializable]
     public class Scale
     {
+        static int length=12;
+        static string[] notes = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+
         public string name;
         public string formula;
-       // public int length;
-       // public string notes;
-       // public string root;
+        public string root;
 
-        public Scale(string n, string f)
+        public Scale(string n, string f, string r)
 
         {
             this.name = n;
             this.formula = f;
+            this.root = r;
         }
 
         public void Print()
         {
-
+            int position = Array.IndexOf(notes, this.root);
+           // string[] s = new string[] { this.formula };
+            int[] seq = new int[20];
+            for (int i = 0; i < this.formula.Length; i++)
+            {
+                seq[i] = Int32.Parse(this.formula[i].ToString());
+                Console.Write(notes[position % length] + " ");
+                position += seq[i];
+            }
+            Console.WriteLine();
         }
     }
 }
